965be83d9ff297feebb765433d2badaf814340c1
savedram <michaelsavedra@gmail.com>
2023-11-28 14:07:16 +0000

    Intstalled langauge server for golang to use in helix.

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,8 @@ in
     nodejs python3 python3Packages.pip shellcheck wdisplays git blueman brightnessctl hyprpaper
     home-manager pavucontrol alsa-utils grim bluez vscode gnome.gnome-boxes shfmt mako slurp 
     wl-clipboard unzip statix nixpkgs-fmt neofetch rofi-wayland libnotify waybar htop postgresql
-    insomnia docker-compose tailscale vscode slack networkmanagerapplet openfortivpn lsd helix
+    insomnia docker-compose tailscale vscode slack networkmanagerapplet openfortivpn lsd helix gopls
+    go python311Packages.ruff-lsp
   ];
 
   virtualisation.docker.enable = true; # Enable Docker
